ec8a989c6e3c0fb921198d01dafa5ac81ccf3d2f
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-06 09:56:29 -0500

    exemple with the Sout (System.out.println())

--- src/App.java
+++ src/App.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World !");
-        // sysout, sout, .....
-        Scanner scanner = new Scanner(System.in);
+        try (// System.out.println("Hello, World !");
+                // sysout, sout, .....
+        Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Saisir le message : ");
+
+            String message = scanner.nextLine();
+
+            System.out.println(message);
+        }
     }
 }
 
