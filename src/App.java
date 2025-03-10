5fbc81d5e2c735afb8038100527fe642ed8f63be
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-10 03:07:15 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -4,13 +4,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello : ");
+        System.out.println("Quel est votre nom : ");
 
-        String message = scanner.nextLine();
+        String message = "Hi, ";
 
+        System.out.println(message + "");
         System.out.println(message);
-
-
+        System.out.println(message);
     }
 }
 
