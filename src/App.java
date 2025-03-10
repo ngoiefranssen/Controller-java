63c6ebbfe8b637da0123d257ecee1e4e7895565c
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-10 07:52:14 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -1,15 +1,13 @@
-import java.io.Console;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String zetor = "zétor";
-        int poulet   = 90000;
-        double Bacon = 0.1;
-        
-        System.out.println("Poulet: " + poulet);
-        System.out.println("Bacon (kg): " + Bacon);
-        System.out.println("Tracteur: " + zetor);
-        System.out.println("Et enfin, un résumé: " + poulet + " " + Bacon + " " + zetor);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int value = Integer.valueOf(scanner.nextInt());
+        System.out.println("Vous avez saisi: " + value);
+
     }
 }
 
