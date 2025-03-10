76ff452d60104c3525f4ef4b2ef0ea6874539586
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-10 03:49:24 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        String name = "Ngoie";
+        int age = 20;
+        double height = 1.7;
+        boolean isStudent = true;
 
-        System.out.println("Quel est votre nom : ");
-
-        String message = "Hi, ";
-
-        System.out.println(message + "");
-        System.out.println(message);
-        System.out.println(message);
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am " + height + "m tall");
+        System.out.println("I am a student: " + isStudent);
     }
 }
 
