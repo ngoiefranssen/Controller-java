fea095f716362bc9a768eed825a99188d2941aec
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-10 04:46:59 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
+import java.io.Console;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String zetor = "zétor";
         int poulet   = 90000;
         double Bacon = 0.1;
-
+        
         System.out.println("Poulet: " + poulet);
         System.out.println("Bacon (kg): " + Bacon);
         System.out.println("Tracteur: " + zetor);
