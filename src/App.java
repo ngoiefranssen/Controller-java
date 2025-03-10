4acf14c4e5f05a5f58e301c4c4a8cc17e9ad0702
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-10 08:37:41 -0400

    Modify

--- src/App.java
+++ src/App.java
@@ -8,6 +8,14 @@ public class App {
         int value = Integer.valueOf(scanner.nextInt());
         System.out.println("Vous avez saisi: " + value);
 
-    }
+        Scanner scanner2  = new Scanner(System.in);
+        System.out.println("Enter the second number: ");
+        int value2 = Integer.valueOf(scanner2.nextInt());
+
+        System.out.println("Vous avez saisi: " + value2);
+        int sum = value + value2;
+        System.out.println("The sum of the two numbers is: " + sum);
+        scanner.close();
 }
 
+}
