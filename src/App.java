e379cb4dd9a01165ca6b8a2cdc1cc95d214b9198
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-21 09:18:27 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         try {
+
             System.out.println("Donner des points : ");
             String input = scanner.nextLine();
 
