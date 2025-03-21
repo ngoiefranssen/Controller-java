c51d61816251ae00085d4d3250b33e0718a6e544
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-21 07:49:55 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -3,46 +3,25 @@ import java.util.Scanner;
 public class App {
     /**
      * The main method serves as the entry point for the application.
-        System.out.println("Enter a string: ");
      */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Quel âge as-tu ? ");
-            String number = scanner.nextLine();
-            System.out.println("Vous avez saisi: " + number);
+            System.out.println("Donnez le premier numéro : ");
+            int number1 = Integer.parseInt(scanner.nextLine());
 
-            int year = Integer.parseInt(number);
+            System.out.println("Donnez le deuxième numéro : ");
+            int number2 = Integer.parseInt(scanner.nextLine());
 
-            switch (year) {
-                case 12:
-                    System.out.println("Vous n'êtes pas un adulte");
-                    break;
-                case 18:
-                    System.out.println("Vous êtes un adulte");
-                    break;
-                case 32:
-                    System.out.println("Vous êtes un adulte");
-                    break;
-                default:
-                    break;
+            if (number1 > number2) {
+                System.out.println("Le nombre plus élevé est le suivant: " + number1);
+            } else if (number1 < number2) {
+                System.out.println("Le nombre plus élevé est le suivant: " + number2);
+            } else {
+                System.out.println("Les deux numéros sont égaux");
             }
-
-        //     System.out.println("Enter an integer: ");
-        //     int length = Integer.parseInt(scanner.nextLine());
-        //     System.out.println("Vous avez saisi: " + length);
-
-        //     System.out.println("Enter a double: ");
-        //     double value = Double.parseDouble(scanner.nextLine());
-        //     System.out.println("Vous avez saisi: " + value);
-
-        //     System.out.println("Donnez un booléen: ");
-        //     boolean bool = Boolean.parseBoolean(scanner.nextLine());
-        //     System.out.println("Vous avez saisi: " + bool);
         } finally {
             scanner.close();
         }
-
-      
     }
 }
