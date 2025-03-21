309c74a17550c11afeb8107fcf8704674c4a42d6
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-21 05:37:50 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -8,23 +8,34 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Donnez une corde: ");
-            String text = scanner.nextLine();
-            System.out.println("Vous avez saisi: " + text);
-            
-            System.out.println("Enter an integer: ");
-            int length = Integer.parseInt(scanner.nextLine());
-            System.out.println("Vous avez saisi: " + length);
+            System.out.println("Donnez une an: ");
+            String number = scanner.nextLine();
+            System.out.println("Vous avez saisi: " + number);
 
-            System.out.println("Enter a double: ");
-            double value = Double.parseDouble(scanner.nextLine());
-            System.out.println("Vous avez saisi: " + value);
+            int year = Integer.parseInt(number);
+            if(year > 0){
+                System.out.println("Le numero saisi est positif");
+            } else if(year < 0) {
+                System.out.println("Le numero saisi est négatif");
+            } else {
+                System.out.println("Le numero saisi est zéro");
+            }
 
-            System.out.println("Donnez un booléen: ");
-            boolean bool = Boolean.parseBoolean(scanner.nextLine());
-            System.out.println("Vous avez saisi: " + bool);
+        //     System.out.println("Enter an integer: ");
+        //     int length = Integer.parseInt(scanner.nextLine());
+        //     System.out.println("Vous avez saisi: " + length);
+
+        //     System.out.println("Enter a double: ");
+        //     double value = Double.parseDouble(scanner.nextLine());
+        //     System.out.println("Vous avez saisi: " + value);
+
+        //     System.out.println("Donnez un booléen: ");
+        //     boolean bool = Boolean.parseBoolean(scanner.nextLine());
+        //     System.out.println("Vous avez saisi: " + bool);
         } finally {
             scanner.close();
         }
+
+      
     }
 }
