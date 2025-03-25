c92bb1552c0d8c4ee3ec1515e27d1a689ca95f2e
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-25 02:31:18 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -7,60 +7,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("Entrez la première chaîne :");
-        String chA = reader.nextLine();
-        System.out.println("Entrez la deuxième chaîne :");
-        String chB = reader.nextLine();
+        System.out.println("Quel est votre age ? ");
+        Integer input = Integer.parseInt(reader.nextLine());
 
-        if (chA.equals(chB) && chB.equals(chA)) {
-            System.out.println("Mêmes");
+        if(input < 0 && !(input > 125)) {
+            System.out.println("OK: ");
         } else {
-            System.out.println("Différents");
-            
+            System.out.println("indesirable : ");
         }
 
         reader.close();
-
-        // try {
-
-        //     System.out.println("Donner des points : ");
-        //     String input = scanner.nextLine();
-
-        //     String[] points = input.split(" ");
-
-        //     for (String point : points) {
-        //         switch (point) {
-        //             case "[0]":
-        //                 System.out.printf("Donner des points: %s\nGrade : Impossible\n", point);
-        //                 break;
-        //             case "[0-49]":
-        //                 System.out.printf("Donner des points: %s\nGrade : échec\n", point);
-        //                 break;
-        //             case "[50-59]":
-        //                 System.out.printf("Donner des points: %s\nGrade : 1\n", point);
-        //                 break;
-        //             case "[60-69]":
-        //                 System.out.printf("Donner des points: %s\nGrade : 2\n", point);
-        //                 break;
-        //             case "[70-79]":
-        //                 System.out.printf("Donner des points: %s\nGrade : 3\n", point);
-        //                 break;
-        //             case "[80-89]":
-        //                 System.out.printf("Donner des points: %s\nGrade : 4\n", point);
-        //                 break;
-        //             case "[90-100]":
-        //                 System.out.printf("Donner des points: %s\nGrade : 5\n", point);
-        //                 break;
-        //             case "[100]":
-        //                 System.out.printf("Donner des points: %s\nGrade : C'est incroyable\n", point);
-        //                 break;
-        //             default:
-        //                 System.out.printf("Donner des points: %s\nGrade : Invalide\n", point);
-        //                 break;
-        //         }
-        //     }
-        // } finally {
-        //     scanner.close();
-        // }
     }
 }
