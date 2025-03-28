535efe69aff2f2b769095a57f848a30aa53a8875
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-28 05:00:40 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -7,21 +7,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
 
+        int one = 0;
+
         while (true) {
-            System.out.println("Input a number ");
+            System.out.println("Input a number one: ");
             int number = Integer.valueOf(reader.nextLine());
-        
-            if (number < 0) {
-                System.out.println("Unfit number");
-                continue;
-            }
         
             if (number == 0) {
                 break;
             }
         
-            System.out.println(number * number);
+            if (number == 1) {
+                one = one + 1;
+            }
+
         }
+        System.out.println("One: " + one);
 
         reader.close();
     }
