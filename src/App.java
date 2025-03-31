5297836d4d6a0427e2bc64316aad61359c1e120c
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-31 04:28:21 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -1,28 +1,10 @@
-import java.util.Scanner;
-
 public class App {
-    /**
-     * The main method serves as the entry point for the application.
-     */
-    public static void main(String[] args) throws Exception {
-        Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a=0;
 
-        int one = 0;
-
-        while (true) {
-            System.out.println("Input a number one: ");
-            int number = Integer.valueOf(reader.nextLine());
-        
-            if (number == 0) {
-                break;
-            }
-            if (number > 0) {
-                one = com.sun(number)
-            }
+        while (a < 10) {
+            System.out.println("Numbre: " + a);
+            a++;
         }
-
-        System.out.println("One: " + one);
-
-        reader.close();
     }
 }
