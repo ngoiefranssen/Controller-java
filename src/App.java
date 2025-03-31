ce361862e5d266c7095f8f2189f41f36ff3724ec
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-03-31 09:34:10 -0400

    correction and modify

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        int a=0;
+        Scanner scanner = new Scanner(System.in);
+        int number;
 
-        while (a < 10) {
-            System.out.println("Numbre: " + a);
-            a++;
+        do {
+            System.out.print("Entrez un nombre (doit être > 0): ");
+            number = Integer.valueOf(scanner.nextLine());
+        } while (number <= 0);
+
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
         }
+
+        scanner.close();
     }
 }
