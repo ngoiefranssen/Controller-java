89efbe9e7e81e5a2aea9ee514e5d37921026c2c8
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-16 08:41:17 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        printHelloWorld();
+        printApp();
     }
 
-    public static void printHelloWorld() {
-        System.out.println("Hello, Word !");
+    public static void printApp() {
+
+        Scanner render = new Scanner(System.in);
+        System.out.println("Entrez un name du programme : ");
+        String show = String.valueOf(render.nextLine());
+
+        if(show.equals(show)) {
+            System.out.println("Le name du programme est la suivante : " + show);
+        }
+
+        System.out.println("Fin du programme. " + show);
+        render.close();
     }
 }
