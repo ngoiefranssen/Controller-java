3443930fe85d6bba463cb8a5e8870e642a46370e
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-16 03:01:12 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -2,23 +2,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        printHelloWorld();
+    }
 
-        int sum;
-
-        while (true) {
-            System.out.print("Saisir un numero: ");
-            int input = Integer.valueOf(reader.nextLine());
-        
-            if (input == -1) {
-                break;
-            }
-           
-        }
-
-        System.out.println("Au revoir");
-        // System.out.println("Sum: " + input);
-
-        reader.close();
+    public static void printHelloWorld() {
+        System.out.println("Hello, World!");
     }
 }
