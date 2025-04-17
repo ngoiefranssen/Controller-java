c62edd6d8545d25a25a097343d4b8a84c1d29f66
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-17 09:49:41 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -4,23 +4,12 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
-        int first = Integer.valueOf(scanner.nextLine());
-
-        System.out.print("Enter the second number: ");
-        int second = Integer.valueOf(scanner.nextLine());
-
-        System.out.print("Enter the third number: ");
-        int third = Integer.valueOf(scanner.nextLine());
-
-        double averageResult = division(first, second, third);
-
-        System.out.print("The average of the numbers: " + averageResult);
-        
+        int sum = onChangeNumber(7, 7, 7, 7);
+        System.out.println("Sum: " + sum);
         scanner.close();
     }
 
-    public static double division(int numerator, int denominator, int other) {
-        return (numerator + denominator + other) / 3.0;
+    public static int onChangeNumber(int number, int operation, int operand, int operand2) {
+        return number + operand + operand2 + operation;
     }
 }
