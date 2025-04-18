4398ee4514b8b70fbee5f9d853bd80a254ac3db4
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-18 09:11:39 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -7,20 +7,31 @@ public class App {
 
     public static void flessmallest() {
         Scanner reader = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;
 
         while (true) {
-            System.out.println("Start number: ");
-            int start = Integer.valueOf(reader.nextLine());
 
-            System.out.println("End number: ");
-            int end = Integer.valueOf(reader.nextLine());
+            System.out.println("-- values: " + values + ", sum: " + sum);
+            System.out.println("Provide a value, a negative value ends the program");
+            int value = Integer.valueOf(reader.nextLine());
 
-            if(start >= end){
-                System.out.println("start " + start );
+            if (value < 0) {
+                System.out.println("-- value is negative, exiting loop");
                 break;
             }
-            start = start - 1;
-            System.out.println(start);
+
+            values = values + 1;
+            sum = sum + value;
+        }
+
+        System.out.println("-- loop exited");
+        System.out.println("-- values: " + values + ", sum: " + sum);
+
+        if (sum == 0) {
+            System.out.println("The average of the values could not be calculated.");
+        } else {
+            System.out.println("Average of values: " + (1.0 * sum / values));
         }
 
         reader.close();
