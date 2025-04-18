38103203e95d9286169a5b447ce27cc5c931e9f5
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-18 08:13:47 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -13,19 +13,21 @@ public class App {
         while (true) {
             System.out.println("Provide a value, a negative value ends the program");
             int value = Integer.valueOf(reader.nextLine());
-            if (value < 0) {
+            if (value > 0) {
                 break;
             }
 
-            values = values + 1;
-            sum = sum + value;
+            values = values - 1;
+            // sum = sum + value;
+            // System.out.println(values);
         }
 
-        if (sum == 0) {
-            System.out.println("The average of the values could not be calculated.");
-        } else {
-            System.out.println("Average of values: " + (1.0 * sum / values));
-        }
+
+        // if (sum == 0) {
+        //     System.out.println("The average of the values could not be calculated.");
+        // } else {
+        //     System.out.println("Average of values: " + (1.0 * sum / values));
+        // }
 
         reader.close();
     }
