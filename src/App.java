b948743409cd364052310e16620fbb1e26e388a5
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-18 05:35:47 -0400

    resolve the bleme

--- src/App.java
+++ src/App.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        reader.close();
+        flessmallest();
     }
 
-    public static void smallest(int number1, int number2, int number3) {
-        
+    public static void flessmallest() {
+        Scanner reader = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Provide a value, a negative value ends the program");
+            int value = Integer.valueOf(reader.nextLine());
+            if (value < 0) {
+                break;
+            }
+
+            values = values + 1;
+            sum = sum + value;
+        }
     }
 }
