2c1f2eef62ad235c4b0992bc525c30edc087192a
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-23 03:45:47 -0400

    resolve

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
@@ -6,35 +6,11 @@ public class App {
     }
 
     public static void flessmallest() {
-        
-        Scanner reader = new Scanner(System.in);
-        int values = 0;
-        int sum = 0;
+        ArrayList<String> fullname = new ArrayList<>();
 
-        while (true) {
+        fullname.add("Ngoie Franssen"); // index 0
+        fullname.add("Kabamba Franssen"); // index 1
 
-            System.out.println("-- values: " + values + ", sum: " + sum);
-            System.out.println("Provide a value, a negative value ends the program");
-            int value = Integer.valueOf(reader.nextLine());
-
-            if (value < 0) {
-                System.out.println("-- value is negative, exiting loop");
-                break;
-            }
-
-            values = values + 1; // result 1
-            sum = sum + value; // sum is value
-        }
-
-        System.out.println("-- loop exited");
-        System.out.println("-- values: " + values + ", sum: " + sum);
-
-        if (sum == 0) {
-            System.out.println("The average of the values could not be calculated.");
-        } else {
-            System.out.println("Average of values: " + (1.0 * sum / values));
-        }
-
-        reader.close();
+        System.out.println(fullname.get(1));
     }
 }
