d0da6c9bc9547f6afd62edab3a2d2c0d2581d913
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-04-25 08:55:31 -0400

    resolve

--- src/App.java
+++ src/App.java
@@ -2,15 +2,19 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        flessmallest();
+        arrayList();
     }
 
-    public static void flessmallest() {
-        ArrayList<String> fullname = new ArrayList<>();
+    public static void arrayList() {
+        
+        ArrayList<Double> doubl = new ArrayList<>();
 
-        fullname.add("Ngoie Franssen"); // index 0
-        fullname.add("Kabamba Franssen"); // index 1
+        Double d = 2.3;
+        Double d_ = 7.7;
 
-        System.out.println(fullname.get(1));
+        Double sum = d + d_;
+        doubl.add(sum);
+
+        System.out.println(doubl);
     }
 }
